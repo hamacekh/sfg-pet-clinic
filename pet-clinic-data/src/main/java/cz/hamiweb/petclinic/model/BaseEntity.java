3c16020f62a869dd22fd01e5526dccdc3564ee6b
Jan Hamáček <hamacekh@gmail.com>
2019-02-05 17:16:31 +0100

    Added BaseEntity.

---
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package cz.hamiweb.petclinic.model;
+
+import java.io.Serializable;
+
+public abstract class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
