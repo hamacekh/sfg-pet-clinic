a8cf7b99b00799eeae8de5ace8d72382aa62edc9
Jan Hamáček <hamacekh@gmail.com>
2019-02-09 22:57:02 +0100

    Fixed data loader not registered to spring ioc container.

--- pet-clinic-data/src/main/java/cz/hamiweb/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/bootstrap/DataLoader.java
@@ -5,7 +5,9 @@ import cz.hamiweb.petclinic.model.Vet;
 import cz.hamiweb.petclinic.services.OwnerService;
 import cz.hamiweb.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
