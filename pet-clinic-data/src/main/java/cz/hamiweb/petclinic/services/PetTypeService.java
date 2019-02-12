70b32403d6d4cfedf9b27c6acb440a8648f89592
Jan Hamáček <hamacekh@gmail.com>
2019-02-12 20:20:36 +0100

    Added PetType service and its Map implementation.

---
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package cz.hamiweb.petclinic.services;
+
+import cz.hamiweb.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,7 @@
+package cz.hamiweb.petclinic.services.map;
+
+import cz.hamiweb.petclinic.model.PetType;
+import cz.hamiweb.petclinic.services.PetTypeService;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType> implements PetTypeService {
+}
