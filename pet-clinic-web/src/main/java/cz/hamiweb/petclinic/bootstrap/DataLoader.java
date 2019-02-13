1bafeb7d315b3ad62db1ad5527c85cc8a0015f38
Jan Hamáček <hamacekh@gmail.com>
2019-02-13 19:44:44 +0100

    Added specialities in bootstrap.

--- pet-clinic-web/src/main/java/cz/hamiweb/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/cz/hamiweb/petclinic/bootstrap/DataLoader.java
@@ -1,9 +1,6 @@
 package cz.hamiweb.petclinic.bootstrap;
 
-import cz.hamiweb.petclinic.model.Owner;
-import cz.hamiweb.petclinic.model.Pet;
-import cz.hamiweb.petclinic.model.PetType;
-import cz.hamiweb.petclinic.model.Vet;
+import cz.hamiweb.petclinic.model.*;
 import cz.hamiweb.petclinic.services.OwnerService;
 import cz.hamiweb.petclinic.services.PetTypeService;
 import cz.hamiweb.petclinic.services.SpecialityService;
@@ -32,6 +29,13 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        int count = petTypeService.findAll().size();
+        if(count == 0) {
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDog = petTypeService.save(dog);
@@ -39,7 +43,6 @@ public class DataLoader implements CommandLineRunner {
         PetType cat = new PetType();
         dog.setName("Cat");
         PetType savedCat = petTypeService.save(dog);
-
 
 
         Owner owner = new Owner();
@@ -71,15 +74,26 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner2);
 
+        Speciality radiology = new Speciality();
+        radiology.setDescription("radiology");
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("dentistry");
+        Speciality surgery = new Speciality();
+        surgery.setDescription("surgery");
+        var savedRadiology = specialityService.save(radiology);
+        var savedDentistry = specialityService.save(dentistry);
+        var savedSurgery = specialityService.save(surgery);
 
         Vet vet1 = new Vet();
         vet1.setFirstName("John");
         vet1.setLastName("Doe");
+        vet1.getSpecialities().add(savedRadiology);
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Micky");
         vet2.setLastName("Mouse");
+        vet2.getSpecialities().add(savedSurgery);
         vetService.save(vet2);
     }
 }
