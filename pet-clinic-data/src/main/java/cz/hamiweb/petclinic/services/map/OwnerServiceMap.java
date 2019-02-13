1b21b06f3dc6f43526965cfc0f1f447dafb41d76
Jan Hamáček <hamacekh@gmail.com>
2019-02-13 17:45:20 +0100

    Fixed Owner and Pet service maps to persist Pets and PetTypes.

--- pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/map/OwnerServiceMap.java
@@ -1,12 +1,37 @@
 package cz.hamiweb.petclinic.services.map;
 
 import cz.hamiweb.petclinic.model.Owner;
+import cz.hamiweb.petclinic.model.Pet;
 import cz.hamiweb.petclinic.services.OwnerService;
+import cz.hamiweb.petclinic.services.PetService;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner> implements OwnerService  {
+
+    private final PetService petService;
+
+    public OwnerServiceMap(PetService petService) {
+        this.petService = petService;
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        if(object != null){
+            if(object.getPets() != null){
+                object.getPets().forEach(pet -> {
+                    if(pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        }else{
+            return null;
+        }
+    }
 
     @Override
     public Owner findByLastName(@NonNull String lastName) {
--- pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/map/PetServiceMap.java
@@ -2,9 +2,29 @@ package cz.hamiweb.petclinic.services.map;
 
 import cz.hamiweb.petclinic.model.Pet;
 import cz.hamiweb.petclinic.services.PetService;
+import cz.hamiweb.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PetServiceMap extends AbstractMapService<Pet> implements PetService {
 
+    private final PetTypeService petTypeService;
+
+    public PetServiceMap(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        if(object != null){
+            if(object.getPetType() != null){
+                if(object.getPetType().getId() == null){
+                    object.setPetType(petTypeService.save(object.getPetType()));
+                }
+            }
+            return super.save(object);
+        }else{
+            return null;
+        }
+    }
 }
