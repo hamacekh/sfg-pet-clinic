84f1a43f063ee75344a5f56328a06867d9677de1
Jan Hamáček <hamacekh@gmail.com>
2019-02-21 22:17:29 +0100

    Changed spring IOC settings to have JPA services as primary.

--- pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/jpa/PetTypeServiceJpa.java
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/jpa/PetTypeServiceJpa.java
@@ -4,9 +4,11 @@ import cz.hamiweb.petclinic.model.PetType;
 import cz.hamiweb.petclinic.repositories.PetTypeRepository;
 import cz.hamiweb.petclinic.services.PetTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class PetTypeServiceJpa extends AbstractCrudService<PetType, PetTypeRepository> implements PetTypeService {
     @Autowired
     public PetTypeServiceJpa(PetTypeRepository repository) {
--- pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/jpa/VetServiceJpa.java
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/jpa/VetServiceJpa.java
@@ -4,9 +4,11 @@ import cz.hamiweb.petclinic.model.Vet;
 import cz.hamiweb.petclinic.repositories.VetRepository;
 import cz.hamiweb.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class VetServiceJpa extends AbstractCrudService<Vet, VetRepository> implements VetService {
 
     @Autowired
