ed469b3834d700d1be1f9a98f13212116135290d
Jan Hamáček <hamacekh@gmail.com>
2019-02-21 21:55:49 +0100

    Added Vet service for JPA. Closes #5.

---
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/jpa/VetServiceJpa.java
@@ -0,0 +1,15 @@
+package cz.hamiweb.petclinic.services.jpa;
+
+import cz.hamiweb.petclinic.model.Vet;
+import cz.hamiweb.petclinic.repositories.VetRepository;
+import cz.hamiweb.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class VetServiceJpa extends AbstractCrudService<Vet, VetRepository> implements VetService {
+
+    @Autowired
+    public VetServiceJpa(VetRepository repository) {
+        super(repository);
+    }
+
+}
