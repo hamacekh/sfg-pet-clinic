d76e0173cb459da5f427237a969ecb04b49abe1f
Jan Hamáček <hamacekh@gmail.com>
2019-02-21 22:11:05 +0100

    Added service annotation to VetService. Closes #5.

--- pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/jpa/VetServiceJpa.java
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/services/jpa/VetServiceJpa.java
@@ -4,7 +4,9 @@ import cz.hamiweb.petclinic.model.Vet;
 import cz.hamiweb.petclinic.repositories.VetRepository;
 import cz.hamiweb.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VetServiceJpa extends AbstractCrudService<Vet, VetRepository> implements VetService {
 
     @Autowired
