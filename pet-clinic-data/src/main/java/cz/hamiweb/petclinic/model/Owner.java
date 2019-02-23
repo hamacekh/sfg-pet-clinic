76a9523b2857b0ea2290dde64f89f2067883ed74
Jan Hamáček <hamacekh@gmail.com>
2019-02-23 10:12:04 +0100

    Added Lombok default builder to Owner class.

--- pet-clinic-data/src/main/java/cz/hamiweb/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/cz/hamiweb/petclinic/model/Owner.java
@@ -1,5 +1,6 @@
 package cz.hamiweb.petclinic.model;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,6 +14,7 @@ import java.util.Set;
 @Entity
 @Table(name = "owners")
 @Data
+@Builder
 @EqualsAndHashCode(callSuper = true)
 public class Owner extends Person{
 
