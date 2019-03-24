2f5b73046c0d7f0494352ab5242ca9bf3e1fdad6
Jan Hamáček <hamacekh@gmail.com>
2019-03-24 10:25:12 +0100

    Created Owner details page. Closes #26

--- pet-clinic-web/src/main/java/cz/hamiweb/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/cz/hamiweb/petclinic/controllers/OwnerController.java
@@ -1,10 +1,17 @@
 package cz.hamiweb.petclinic.controllers;
 
+import cz.hamiweb.petclinic.model.Owner;
 import cz.hamiweb.petclinic.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping({"owners"})
@@ -26,6 +33,20 @@ public class OwnerController {
     @RequestMapping({"find", "find/index", "find/index.html"})
     public String findOwners(){
         return "notImplemented";
+    }
+
+    @GetMapping({"/{ownerId}"})
+    public ModelAndView getOwnerDetail(@PathVariable("ownerId") long ownerId){
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        Optional<Owner> res = ownerService.findById(ownerId);
+        if(res.isEmpty()) {
+            modelAndView.setStatus(HttpStatus.NOT_FOUND);
+            modelAndView.setViewName("errors/notFound");
+            return modelAndView;
+        }else{
+            modelAndView.addObject("owner", res.get());
+        }
+        return modelAndView;
     }
 
 }
--- pet-clinic-web/src/test/java/cz/hamiweb/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/cz/hamiweb/petclinic/controllers/OwnerControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -46,5 +47,21 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners")).andExpect(status().isOk())
                 .andExpect(model().attribute("owners", hasSize(2)))
                 .andExpect(view().name("owners/index"));
+    }
+
+    @Test
+    void getOwnerDetail() throws Exception {
+        Owner owner = Owner.builder().id(2L).firstName("John").lastName("Doe").build();
+        when(ownerService.findById(2L)).thenReturn(Optional.of(owner));
+        mockMvc.perform(get("/owners/2")).andExpect(status().isOk())
+                .andExpect(model().attribute("owner", owner))
+                .andExpect(view().name("owners/ownerDetails"));
+    }
+
+    @Test
+    void getOwnerDetailNotFound() throws Exception{
+        when(ownerService.findById(5L)).thenReturn(Optional.empty());
+        mockMvc.perform(get("/owners/5")).andExpect(status().isNotFound())
+        .andExpect(view().name("errors/notFound"));
     }
 }
